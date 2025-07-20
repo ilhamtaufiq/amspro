bf86deab4878aa36209aee202657b65aa7843afa
Ilham Taufiq <ilhamtaufiq97@gmail.com>
2025-07-20 08:20:54 +0700

    forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
+
         Inertia::share([
             'tahun_aktif' => fn () => session('tahun', now()->year),
         ]);
-          
     }
 }
