fa95c228e0864a44f0a3de30bf34a90cdb6c79c9
Ilham Taufiq <ilhamtaufiq97@gmail.com>
2025-07-20 10:21:02 +0700

    fix: move Cloudflare Web Analytics script to the correct position in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,9 +10,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
         <!-- Scripts -->
-        <!-- Cloudflare Web Analytics -->
-        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "56af73e195a3433495a17937f5bdfbbb"}'></script>
-        <!-- End Cloudflare Web Analytics -->
+        
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
@@ -20,5 +18,8 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
+        <!-- Cloudflare Web Analytics -->
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "56af73e195a3433495a17937f5bdfbbb"}'></script>
+        <!-- End Cloudflare Web Analytics -->
     </body>
 </html>
