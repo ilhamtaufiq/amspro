1b1579946ef0877374229bed09c70f8aa4b18635
Ilham Taufiq <ilhamtaufiq97@gmail.com>
2025-07-21 12:40:11 +0700

    refactor: Remove commented-out registration routes in auth.php and clean up Login component

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -97,12 +97,7 @@ export default function Login({
                                 Login
                             </Button>
                         </div>
-                        <div className="mt-4 text-center text-sm">
-                            Don&apos;t have an account?{" "}
-                            <Link href="/register" className="underline">
-                                Sign up
-                            </Link>
-                        </div>
+                        
                     </CardContent>
                 </Card>
             </form>
